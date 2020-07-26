a8b9d823858fbda581a0799ceab22bf12957e00b
Shubhankar <deshmukhshubhankar@gmail.com>
2020-07-26 13:11:25 +0530

    First Commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class User {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    String id;
+    String username;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
